fae3dae3fd43c9567d363d48458d42f143c5cff8
User Name <99ww77@gmail.com>
2021-06-12 10:47:22 +0300

    second commit

--- main.py
+++ main.py
@@ -3,19 +3,20 @@ from datetime import datetime as dt
 import os
 
 
-def decorator_function(fn):
+def parametrized_decor(parameter):
+    def decorator_function(fn):
+        def foo(*args):
+            logging.basicConfig(filename=os.path.join(parameter, 'logger.log'), level=logging.INFO)
+            result = fn(*args) + '!'
+            logging.info([str(dt.now())[:-7], fn.__name__, *args, result])
+            return result
+        return foo
+    return decorator_function
 
-    def foo(*args, **kwargs):
-        path = kwargs['path']
-        logging.basicConfig(filename=os.path.join(path, 'logger.log'), level=logging.INFO)
-        logging.info([str(dt.now())[:-7], fn.__name__, *args, fn(*args) + '!'])
-        return fn(*args) + '!'
-    return foo
 
-
-@decorator_function
+@parametrized_decor(parameter='C:\\Logs')
 def func(a):
     return 'Hello ' + a
 
 
-print(func('Decorator',  path='C:\\Logs'))
+print(func('Decorator'))
